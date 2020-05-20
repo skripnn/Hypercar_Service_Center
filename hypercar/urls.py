36d7b96bc079612a7495d8ecb32332116393f49d
Aleksandr Neumoin <63591184+skripnn@users.noreply.github.com>
2020-05-20 18:36:04 +0300

    Hypercar Service Center - 2/5

--- hypercar/urls.py
+++ hypercar/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
-from tickets.views import WelcomeView
+from tickets.views import WelcomeView, MenuView
 
 
 urlpatterns = [
     path('welcome/', WelcomeView.as_view()),
+    path('menu/', MenuView.as_view())
 ]
--- tickets/views.py
+++ tickets/views.py
@@ -1,7 +1,18 @@
 from django.views import View
 from django.http.response import HttpResponse
+from django.shortcuts import render
 
 
 class WelcomeView(View):
     def get(self, request, *args, **kwargs):
         return HttpResponse('<h2>Welcome to the Hypercar Service!</h2>')
+
+
+class MenuView(View):
+    menu = {'change_oil': 'Change oil',
+            'inflate_tires': 'Inflate tires',
+            'diagnostic': 'Get diagnostic test'}
+    template_name = 'tickets/base.html'
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name, context={'menu': self.menu})
